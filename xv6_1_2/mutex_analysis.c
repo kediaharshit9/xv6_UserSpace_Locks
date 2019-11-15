d0ff516b5ff6653779652bca35aa58f5c22afbe0
Mohit Singla <mohitsingla1098@gmail.com>
2019-11-15 15:35:23 +0530

    mutex analysis

--- xv6_1_2/mutex_analysis.c
+++ xv6_1_2/mutex_analysis.c
@@ -6,66 +6,56 @@ int
 main(int argc, char *argv[])
 {
 	counter_init();
-	// int rc = fork();
 	int y=uptime();
 
-	// int x;
+	int x;
 	if(fork()==0)
 	{
-
-
-		for(int i=0; i<1000000; i++)
+		for(int i=0; i<100000; i++)
 		{
-			// my_lock();
-			// x=counter_get();
-			// counter_set(x+1);	
-			// my_unlock();
-			printf(1, "%d\n",uptime());
-			// }
+			my_lock();
+			x=counter_get();
+			counter_set(x+1);	
+			my_unlock();
 		}
-		// printf(1, "%d %d\n",getpid(), counter_get());
 	}
 	else
 	{	
 		if(fork()==0)
 		{
-			for(int i=0; i<1000000; i++)
+			for(int i=0; i<100000; i++)
 			{
-				// my_lock();
-				// x=counter_get();
-				// counter_set(x+1);	
-				// my_unlock();
-				// printf(1, "%d\n",uptime());
+				my_lock();
+				x=counter_get();
+				counter_set(x+1);	
+				my_unlock();
 			}
-			// printf(1, "%d %d\n",getpid(), counter_get());
 		}
 		else
 		{
-
 			if(fork()==0)
 			{
-
-
-				for(int i=0; i<1000000; i++)
+				for(int i=0; i<100000; i++)
 				{
-					// my_lock();
-					// x=counter_get();
-					// counter_set(x+1);	
-					// my_unlock();
+					my_lock();
+					x=counter_get();
+					counter_set(x+1);	
+					my_unlock();
 				}
-				// printf(1, "%d %d\n",getpid(), counter_get());
 			}
 			else
 			{		
 				for(int i=0; i<100000;i++)
 				{
-					// my_lock();
-					// x=counter_get();
-					// counter_set(x+1);
-					// my_unlock();
+					my_lock();
+					x=counter_get();
+					counter_set(x+1);
+					my_unlock();
 				}	
-				// printf(1, "%d %d\n",getpid(), counter_get());
-				wait();wait();wait();
+				printf(1, "%d\n", counter_get());
+				wait();
+				wait();
+				wait();
 				printf(1, "%d\n",uptime()-y);
 			}
 	    }
